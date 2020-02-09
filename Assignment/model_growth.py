9c4cfa8e67eb1c2be05eca327adcc2d8442f1eae
linamnt <10761918+linamnt@users.noreply.github.com>
2020-02-09 15:06:51 -0500

    update model_growth and add images

--- Assignment/model_growth.py
+++ Assignment/model_growth.py
@@ -1,32 +1,34 @@
-**a) ....**
-import numpy as np
+# import libraries used in this script
+**(a, 1 mark)  ....**
 
 def main():
+    # read in the data from the .csv passed to our script
+    filename = **(b, 0.5 marks)...**
+    experiment_data = **(d, 0.5 marks)...**
 
-    filename = **b)...**
-    rate_params = **c)....**
+    # store the command-line arguments that represent the start, stop, and step size in the variable rate_params
+    rate_params = **(c, 0.5 marks)....**
+    # note: since the parameters that are read by sys.argv are strings, we have to convert them to floats using float(a_string)
+    range_rate = np.arange(float(rate_params[0]), float(rate_params[1]), float(rate_params[2]))
 
-
-    range_rate = np.arange(float(rate_params[0]),float(rate_params[1]),float(rate_params[2])) # note here we made a slight change: since the parameters that are read by sys library are strings, we have to convert them to floats.
-    experiment_data = **d)...**
-
-    # define a function that calcualates mean squared error
-    def squared_error(prediction,data):
+    # define a function that calculates mean squared error
+    def squared_error(prediction, data):
         residual = prediction - data
-        mse = (residual**2).mean()          #or np.sum(residual**2)
+        mse = (residual**2).mean()          
         return mse
 
-    # try the parameters and choose the one with the least squared error
+    # try the parameters and choose the one with the smallest squared error
     mse = []
     # note two modifications below: we are taking the N0 and the time vector from data
     N0 = experiment_data[0]
-    t = np.linspace(0,10,len(experiment_data))
+    t = np.linspace(0, 10, len(experiment_data))
     for r in range_rate:
         prediction = N0*np.exp(r*t)
-        mse.append(squared_error(prediction,experiment_data))
+        mse.append(squared_error(prediction, experiment_data))
 
     best_fit = range_rate[np.argmin(mse)]
-    print('We predict the rate of growth of this bacterial population to be',best_fit)
-
-**e)...**
-**d)...**
+    print('We predict the rate of growth of this bacterial population to be', best_fit)
+    
+    
+# write the code necessary to make sure the main() function is called when we run the script from command line
+**(e, 1 mark)...**
