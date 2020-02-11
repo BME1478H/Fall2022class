ce442624d79aa4ad3323076af3216adff9306467
nguyenjenny <nguyenpjenny@gmail.com>
2020-02-11 01:09:38 -0500

    Updated model_growth.py with answers

--- Assignment/model_growth.py
+++ Assignment/model_growth.py
@@ -1,13 +1,16 @@
 # import libraries used in this script
-**(a, 1 mark)  ....**
+import sys #**(a, 1 mark)  ....**
+import numpy as np
+
+
 
 def main():
     # read in the data from the .csv passed to our script
-    filename = **(b, 0.5 marks)...**
-    experiment_data = **(c, 0.5 marks)...**
+    filename =  sys.argv[1]#**(b, 0.5 marks)...**
+    experiment_data = np.loadtxt(filename, delimiter=",") #**(c, 0.5 marks)...**
 
     # store the command-line arguments that represent the start, stop, and step size in the variable rate_params
-    rate_params = **(d, 0.5 marks)....**
+    rate_params = sys.argv[2:] #**(d, 0.5 marks)....**
     # note: since the parameters that are read by sys.argv are strings, we have to convert them to floats using float(a_string)
     range_rate = np.arange(float(rate_params[0]), float(rate_params[1]), float(rate_params[2]))
 
@@ -31,4 +34,6 @@ def main():
     
     
 # write the code necessary to make sure the main() function is called when we run the script from command line
-**(e, 1 mark)...**
+
+if __name__ == "__main__": # **(e, 1 mark)...**
+    main()
